346af4d5598e13c96bb10ba7451eeaf2073cf405
andd <andd@etc.vn>
2023-11-21 13:12:42 +0700

    jobs

---
+++ src/data/index.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    id: "01",
+    title: "Kinh doanh",
+    contents: [
+      "1368 Việc Làm",
+      "Mức lương trung bình:",
+      "4-21 triệu VNĐ/tháng.",
+    ],
+  },
+  {
+    id: "02",
+    title: "Công nghệ thông tin",
+    contents: [
+      "836 Việc Làm",
+      "Mức lương trung bình:",
+      "13.8-50 triệu VNĐ/tháng.",
+    ],
+  },
+  {
+    id: "03",
+    title: "Kế toán / kiểm toán",
+    contents: ["675 Việc Làm", "Mức lương trung bình:", "9-13 triệu VND/tháng"],
+  },
+  {
+    id: "04",
+    title: "Ngân hàng",
+    contents: [
+      "518 việc làm",
+      "Mức lương trung bình:",
+      "12-20 triệu VND/tháng",
+    ],
+  },
+  {
+    id: "05",
+    title: "Kiến trúc/Xây dựng",
+    contents: [
+      "468 Việc Làm",
+      "Mức lương trung bình:",
+      "10-30 triệu VND/tháng",
+    ],
+  },
+  {
+    id: "06",
+    title: "Bất động sản",
+    contents: [
+      "90 việc làm",
+      "Mức lương trung bình:",
+      "3.5 - 30 triệu VND/tháng",
+    ],
+  },
+];
